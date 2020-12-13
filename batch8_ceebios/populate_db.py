566609898c6148571472ac79fb1804ec793ed362
Christian MARECHAL <christian.marechal@it4pme.fr>
2020-12-13 19:31:09 +0100

    how to open connnectionwith mongodb

--- batch8_ceebios/populate_db.py
+++ batch8_ceebios/populate_db.py
@@ -5,23 +5,42 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
+
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_PORT = 27017
+MONGO_dbname = 'ceebios'
 MONGO_UNSERNAME = os.getenv("MONGO_USERNAME")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 
-client = MongoClient(
-    f"mongodb://{MONGO_UNSERNAME}:{MONGO_PASSWORD}@165.22.121.95:27017/"
-)
+
+client = MongoClient(host=MONGO_HOST, port=MONGO_PORT, username=MONGO_UNSERNAME,  password=MONGO_PASSWORD)
+db1 = client[MONGO_dbname]
+
+# now you have one cursor for collection species, and a second for documents
+
+cursor_species = db1['species']
+cursor_documents = db1['documents']
+
+print('- cursors to write or find into Mongodb:')
+print('cursor_species', cursor_species)
+print('cursor_documents', cursor_documents)
 
 db = client.documents
 print(db)
 
-posts = db.posts
+
+posts = db.posts # ? ? i don't understand i don't see such collection
 
 for file in os.listdir("/Volumes/Extreme SSD/ceebios"):
     if file.endswith(".json"):
         with open(os.path.join("/Volumes/Extreme SSD/ceebios", file)) as json_file:
             new_posts = json.load(json_file)
-    result = posts.insert_many(new_posts)
+
+            
+            # result = posts.insert_many(new_posts)
+            # logiquement on insert ici l'article 
+            result = cursor_documents.insert_many(new_posts)
+    
     print("Number of inserted articles: ")
     print(len(result.inserted_ids))
     print(f"done with {file}")
@@ -30,3 +49,6 @@ running_sum = 0
 for post in posts.find():
     running_sum += 1
 print(running_sum)
+
+# fermeture 
+client.close()
